1adb8a7b21c50cea5ffd216971441f71b118f099
flauta <primiani@dag.it>
2022-08-01 13:59:29 +0000

    aggiunta funzione per disegnare valore

--- main.ts
+++ main.ts
@@ -3,12 +3,24 @@ input.onButtonPressed(Button.A, function () {
     if (nota > 6) {
         nota = 0
     }
-    basic.showString("" + (text_list[nota]))
+    basic.showString("" + (nomi_note[nota]))
 })
-let text_list: string[] = []
+input.onButtonPressed(Button.B, function () {
+    valore += 1
+    if (valore > 3) {
+        valore = 0
+    }
+    disegna_valore(valore)
+})
+function disegna_valore (fig: number) {
+    valori[fig].showImage(0)
+}
+let valore = 0
+let valori: Image[] = []
+let nomi_note: string[] = []
 let nota = 0
 nota = -1
-text_list = [
+nomi_note = [
 "a",
 "b",
 "c",
@@ -16,4 +28,34 @@ text_list = [
 "e",
 "f",
 "g"
+]
+valori = [
+images.createImage(`
+    . . . . .
+    . . . . .
+    . # # . .
+    # . . # .
+    . # # . .
+    `),
+images.createImage(`
+    . . . # .
+    . . . # .
+    . # # # .
+    # . . # .
+    . # # . .
+    `),
+images.createImage(`
+    . . . # .
+    . . . # .
+    . # # # .
+    # # # # .
+    . # # . .
+    `),
+images.createImage(`
+    . . # . .
+    . . # # .
+    . # # . #
+    # . # . .
+    . # . . .
+    `)
 ]
